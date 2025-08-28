bb5f4cda510606905b7f47ca1d8718195efb338f
hyun0-25 <lisa1072@skku.edu>
2025-08-28 23:15:38 +0900

    [Feat] 유저 회원가입 body 미입력시 예외처리
    USER_NAME_NOT_ALLOWED

--- backend/src/main/java/com/project/backend/users/service/UserService.java
+++ backend/src/main/java/com/project/backend/users/service/UserService.java
@@ -1,8 +1,10 @@
 package com.project.backend.users.service;
 
+import com.project.backend.global.exception.BaseException;
 import com.project.backend.users.domain.User;
 import com.project.backend.users.dto.request.UserRequestDto;
 import com.project.backend.users.dto.response.UserResponseDto;
+import com.project.backend.users.exception.UserErrorCode;
 import com.project.backend.users.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,6 +20,8 @@ public class UserService {
 
     public UserResponseDto createUser(UserRequestDto userRequestDto) {
         log.info("{ UserService } : user 생성");
+        if (userRequestDto.userName() == null || userRequestDto.userName().equals(""))
+            throw BaseException.type(UserErrorCode.USER_NAME_NOT_ALLOWED);
         User user = User.createUser(userRequestDto.userName());
         userRepository.save(user);
         log.info("{ UserService } : user 생성 성공");
