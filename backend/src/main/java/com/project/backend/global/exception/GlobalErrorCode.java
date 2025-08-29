e1645bbf240e1c2db14db4a375e0cc4aebb680de
hyun0-25 <lisa1072@skku.edu>
2025-08-29 23:02:02 +0900

    [Feat] global - soft delete, error code 구현

---
+++ backend/src/main/java/com/project/backend/global/exception/GlobalErrorCode.java
@@ -0,0 +1,20 @@
+package com.project.backend.global.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public enum GlobalErrorCode implements ErrorCode{
+    OBJECT_ALREADY_DELETED(HttpStatus.BAD_REQUEST, "REQUEST_001", "이미 삭제된 객체입니다.")
+    ;
+
+    private final HttpStatus status;
+    private final String errorCode;
+    private final String message;
+
+    GlobalErrorCode(HttpStatus status, String errorCode, String message) { //BaseResponseStatus 에서 각 해당하는 코드를 생성자로 맵핑
+        this.status = status;
+        this.errorCode = errorCode;
+        this.message = message;
+    }
+}
