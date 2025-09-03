e2060fa0005b4c307103814be86025e774ed51dc
gywns2zang9 <gywns2zang9@gmail.com>
2025-09-03 21:19:01 +0900

    [Feat] 모바일 레이아웃 구현

---
+++ frontend/app/src/App.tsx
@@ -0,0 +1,12 @@
+import MobileFrame from "./layouts/MobileFrame";
+
+function App() {
+  return (
+    <MobileFrame>
+      <div style={{ padding: 32, textAlign: "center" }}>
+      </div>
+    </MobileFrame>
+  );
+}
+
+export default App;
