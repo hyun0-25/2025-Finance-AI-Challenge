d07b974d64cb7957bd68771d55dc5721a9727f92
gywns2zang9 <gywns2zang9@gmail.com>
2025-09-16 23:16:56 +0900

    Feat: 마이페이지 카드 이미지 추가

--- frontend/app/src/pages/MyPage.tsx
+++ frontend/app/src/pages/MyPage.tsx
@@ -275,7 +275,11 @@ const MyPage: React.FC = () => {
 
                     }}>
                       <img
-                        src="/card-img.png"
+                        src={
+                          card.cardId === 3? "/src/assets/cards/DeepOil.png" :
+                          card.cardId === 2 ? "/src/assets/cards/ANNIVERSE.png" :
+                          "/card-img.png"
+                        }
                         alt="카드 이미지"
                         style={{
                           width: '100%',
